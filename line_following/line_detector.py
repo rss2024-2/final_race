1b8a667b139ab68ac06f8eaaa6685563750fb546
ningshan <ningshan@mit.edu>
2024-04-30 20:36:59 +0000

    added entry point, change package names

--- line_following/line_detector.py
+++ line_following/line_detector.py
@@ -32,7 +32,7 @@ class LineDetector(Node):
         self.image_sub = self.create_subscription(Image, "/zed/zed_node/rgb/image_rect_color", self.image_callback, 5)
         self.bridge = CvBridge() # Converts between ROS images and OpenCV Images
 
-        self.get_logger().info("Cone Detector Initialized")
+        self.get_logger().info("Line Detector Initialized")
 
     def image_callback(self, image_msg):
         # Apply your imported color segmentation function (cd_color_segmentation) to the image msg here
